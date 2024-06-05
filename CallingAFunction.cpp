99234dd981a2d550bbffde211c8c43dda453dc96
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2024-06-05 19:50:55 -0400

    Create CallingAFunction.cpp
    How to call a function in cpp.

---
+++ CallingAFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int getValueFromUser()
+{
+  cout << Enter an integer: ";
+  int input{};
+  cin >> input;
+
+  return input;
+}
+
+int main()
+{
+  int x{ getValueFromUser() };
+  int y{ getValueFromUser() };
+  
+  cout << x << " + " << y << << " = " << x + y << '\n';
+  
+  return 0;
+}
