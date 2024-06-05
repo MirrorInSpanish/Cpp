115f9feb386db70d085c55a50a00316ff6d0efe3
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2024-06-05 18:25:41 -0400

    Create TemperatureConverter.cpp
    Temperatue converter, which takes an input value for fahrenheit and outputs a value for celsius.

---
+++ TemperatureConverter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//Outputs "Enter value for Fahrenheit to convert into Celsius: "
+  cout  << "Enter value for Fahrenheit to convert into Celsius: ";
+
+//Initializes variable f as a float
+  double f;
+
+//Asks user to input a variable and assigns that variable to f
+  cin   >> f;
+
+//Outputs the value of f subtracted by 32 multipled by the value of 5 divided by 9, which is the celsius conversion  
+  cout  << (f - 32) * 5/9 << endl;
+  
+  return 0;
+}
