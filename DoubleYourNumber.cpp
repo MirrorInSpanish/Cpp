057abad6c6a5aba1a685c35115c65f1b33146566
Andrew Espejo <166669925+MirrorInSpanish@users.noreply.github.com>
2024-06-05 20:00:46 -0400

    Create DoubleYourNumber.cpp
    A program for doubling a given integer and outputing the value.
    A program for trippling a given integer and outputing the value.

---
+++ DoubleYourNumber.cpp
@@ -0,0 +1,19 @@
+//A program for doubling a given integer and outputing the value
+//A program for trippling a given integer and outputing the value
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Enter an integer: ";
+
+  int num{};
+  cin >> num;
+
+  cout << "Double " << num << " is: " << num * 2 << endl;
+  cout << "Triple " << num << " is: " << num * 3 >> endl;
+
+  return 0;
+}
